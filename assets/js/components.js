ee411b22b177f564e983f8105824ed743b366546
abelsan <abel@mit.edu>
2024-07-28 09:24:30 -0400

    daily in menu

--- assets/js/components.js
+++ assets/js/components.js
@@ -35,7 +35,7 @@ class NavBar extends HTMLElement {
                 <a class="nav-link" href="#/books">Books</a>
               </li>                             
               <li class="nav-item">
-                <a class="nav-link" href="#/podcast">Podcast</a>
+                <a class="nav-link" href="#/podcast">Daily</a>
               </li>               
             </ul>
           </div>
