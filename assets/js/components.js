bff288047a653487ea44fd54ecaf48be21f7ebfb
abelsan <abel@mit.edu>
2024-08-02 17:08:05 -0400

    abelsan update

--- assets/js/components.js
+++ assets/js/components.js
@@ -49,5 +49,88 @@ class NavBar extends HTMLElement {
 customElements.define('abel-navbar', NavBar);
 
 
+// Create a class for the element
+class Card extends HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+    
+    // attribute content 
+    let title        = this.getAttribute('title');    
+    let img          = this.getAttribute('img');
+    let newsletter   = this.getAttribute('newsletter');
+    let podcast      = this.getAttribute('podcast');
+    let video        = this.getAttribute('video');
+    let description  = this.getAttribute('description');                
 
+    // set title
+    if (!title) {
+        title = '<h5 class="card-title">No Title</h5>'
+    }
+    else{
+        title = `<h5 class="card-title">${title}</h5>`
+    }
+
+    // set image
+    if (!img) {
+        img = `<img src="assets/img/no_image.jpeg" class="img-fluid rounded-start w-100 h-100" alt="..." style="object-fit: cover;">`
+    }
+    else{
+        img = `<img src="${img}" class="img-fluid rounded-start w-100 h-100" alt="..." style="object-fit: cover;">`
+    }
+
+    // set newsletter
+    if (!newsletter) {
+      newsletter = '';
+    }
+    else{
+      newsletter = `<a href="${newsletter}" class="card-link">Newsletter</a>`;
+    }
+
+    // set podcast
+    if (!podcast) {
+      podcast = '';
+    }
+    else{
+      podcast = `<a href="${podcast}" class="card-link">Podcast</a>`;
+    }
+
+    // set video
+    if (!video) {
+      video = '';
+    }
+    else{
+      video = `<a href="${video}" class="card-link">Video</a>`;
+    }
+
+    // set description
+    if (!description) {
+        description = `<p class="card-text">No description</p>`;
+    }
+    else{
+        description = `<p class="card-text">${description}</p>`;
+    }
+
+    this.innerHTML = `
+        <div class="card mb-3 w-100">
+          <div class="row g-0">
+            <div class="col-3">
+              ${img}
+            </div>
+            <div class="col-9 d-flex align-items-center">
+              <div class="card-body">
+                ${title}
+                ${description}
+                ${newsletter}${podcast}${video}
+              </div>
+            </div>
+          </div>
+        </div>    
+    `;    
+
+  }
+}
+
+// Define the new element
+customElements.define('abel-card', Card);
 
